f74f817ae7730397b0335b968d0e81e86efd4752
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2022-05-12 01:21:38 -0300

    refactor: Reorganizando diretórios do projeto

--- MultitierMVC/src/MVC.App/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC.App.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public int ErroCode { get; set; }
-        public string Titulo { get; set; }
-        public string Mensagem { get; set; }
-    }
-}
